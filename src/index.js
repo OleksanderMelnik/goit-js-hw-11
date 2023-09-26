30334555930af11745598a594465d83fc09c9879
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-09-26 21:34:11 +0300

    key-apy

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// import { fetchBreeds } from "./key-api";
+
+
+
+// fetchBreeds().then(data => {
+//     loader.classList.add('loader-text')
+//     data.map(e => {
+//         breedArr.push({
+//             text: e.name, 
+//             value: e.id});
+//     });  
+    
+//     new SlimSelect({
+//         select: '.breed-select',
+//         data: breedArr
+//     });
+    
+//     })
+
+
+
+
+
