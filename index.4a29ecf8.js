23e539a9fbca9421cd9b1326c01a052525013cc4
OleksanderMelnik <131677126+OleksanderMelnik@users.noreply.github.com>
2023-09-26 18:35:44 +0000

    Deploying to gh-pages from @ OleksanderMelnik/goit-js-hw-11@e8d6199217821391e0650443fb4b37b5425d0a6e 🚀

---
+++ index.4a29ecf8.js
@@ -0,0 +1,2 @@
+API_KEY="39676340-6e766954fc3fa698c8d5ed3b9",fetch("https://pixabay.com/api/?key=39676340-6e766954fc3fa698c8d5ed3b9").then((e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})).then((e=>{console.log(e)}));
+//# sourceMappingURL=index.4a29ecf8.js.map
---
+++ index.8aa8f962.js
@@ -0,0 +1,2 @@
+API_KEY="39676340-6e766954fc3fa698c8d5ed3b9",fetch("".concat("https://pixabay.com/api/","?key=").concat(API_KEY)).then((function(t){if(!t.ok)throw new Error(t.statusText);return t.json()})).then((function(t){console.log(t)}));
+//# sourceMappingURL=index.8aa8f962.js.map
