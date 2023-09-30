f088d7b186781ddbff8a45f00ced9170e2019686
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-09-30 13:04:33 +0300

    index scss

--- src/index.js
+++ src/index.js
@@ -80,22 +80,23 @@ function renderMarkingToGallery(images) {
     const markup = images.map(image => { const {id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
       return `
       <div class="photo-card" id="${id}">
-      <a class="gallery__link" href="${largeImageURL}">
-      <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-      </a>   
-        <div class="info">
+      <a href="${largeImageURL}">
+      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
         <p class="info-item"><b>Likes</b>${likes}</p>
         <p class="info-item"><b>Views</b>${views}</p>
         <p class="info-item"><b>Comments</b>${comments}</p>
         <p class="info-item"><b>Downloads</b>${downloads}</p>
       </div>
+     
       </div>
        
       `;
     })
     .join('');  
     containerGallery.insertAdjacentHTML('beforeend', markup);
-
+    lightbox.refresh();
 }
 
 
